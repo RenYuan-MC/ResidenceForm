b9efd02c7deca90bb393cf1c29f647657232d9fd
ChunghwaMC <92585552+ChunghwaMC@users.noreply.github.com>
2025-01-27 15:43:41 +0800

    新增 zh_TW 註解

--- src/main/java/ltd/rymc/form/residence/configs/Config.java
+++ src/main/java/ltd/rymc/form/residence/configs/Config.java
@@ -10,7 +10,7 @@ import space.arim.dazzleconf.annote.ConfSerialisers;
 import space.arim.dazzleconf.sorter.AnnotationBasedSorter;
 
 @SuppressWarnings("unused")
-@ConfHeader("# 配置文件 ResidenceForm Config ver 2.1.0 by RENaa_FD")
+@ConfHeader("# 配置文件 設定檔 ResidenceForm Config ver 2.1.0 by RENaa_FD")
 @ConfSerialisers(LanguageSerialiser.class)
 public interface Config {
 
@@ -19,6 +19,7 @@ public interface Config {
     @ConfComments({
             "",
             "# 启用数据统计",
+            "# 啟用資料統計",
             "# Enable metrics"
     })
     @AnnotationBasedSorter.Order(10)
@@ -28,8 +29,9 @@ public interface Config {
     @ConfKey("language")
     @ConfComments({
             "",
-            "# 语言设置,目前可用语言: en_US, zh_CN",
-            "# Language settings, currently available languages: en_US, zh_CN"
+            "# 语言设置,目前可用语言: en_US, zh_CN, zh_TW",
+            "# 設定語言,目前可用的語言: en_US, zh_CN, zh_TW",
+            "# Language settings, currently available languages: en_US, zh_CN, zh_TW"
     })
     @AnnotationBasedSorter.Order(20)
     Language language();
@@ -39,6 +41,7 @@ public interface Config {
     @ConfComments({
             "",
             "# 将打开表单的指令hook到Residence中",
+            "# 將打開選單的指令套到 Residence 中",
             "# Hook the command to open the form into Residence"
     })
     @AnnotationBasedSorter.Order(40)
