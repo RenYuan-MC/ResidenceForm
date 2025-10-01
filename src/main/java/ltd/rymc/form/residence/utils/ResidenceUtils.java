aa9987e1a84f5cf2d558e7bbd02b0d027b3fe2e3
RENaa_FD <92320175+lRENyaaa@users.noreply.github.com>
2025-10-01 14:13:29 +0800

    修复ResidenceUtils

--- src/main/java/ltd/rymc/form/residence/utils/ResidenceUtils.java
+++ src/main/java/ltd/rymc/form/residence/utils/ResidenceUtils.java
@@ -8,62 +8,142 @@ import com.bekvon.bukkit.residence.protection.FlagPermissions;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ResidenceUtils {
+
+    /**
+     * 获取领地权限
+     * 参考实现 com.bekvon.bukkit.residence.gui.setFlagInfo#recalculateResidence()
+     *
+     * @param player    操作者
+     * @param residence 领地
+     * @return 领地权限列表
+     */
     public static HashMap<String, FlagPermissions.FlagState> getResidenceFlags(Player player, ClaimedResidence residence) {
-        if (residence == null) return new HashMap<>();
-        List<String> flags = residence.getPermissions().getPosibleFlags(player, true, false);
+
+        // TODO: 检查res admin项
+        List<String> flags = residence.getPermissions().getPossibleFlags(player, true, false);
+
         Map<String, Boolean> resFlags = new HashMap<>();
         Map<String, FlagPermissions.FlagState> TempPermMap = new LinkedHashMap<>();
+
         Map<String, Boolean> globalFlags = Residence.getInstance().getPermissionManager().getAllFlags().getFlags();
+
         for (Map.Entry<String, Boolean> one : residence.getPermissions().getFlags().entrySet()) {
-            if (flags.contains(one.getKey())) resFlags.put(one.getKey(), one.getValue());
-        }
-        for (Map.Entry<String, Boolean> one : globalFlags.entrySet()) {
-            String name = one.getKey();
-            Flags flag = Flags.getFlag(name);
-            if (flag != null && !flag.isGlobalyEnabled()) continue;
-            if (!flags.contains(name)) continue;
-            if (resFlags.containsKey(name)) {
-                TempPermMap.put(name, resFlags.get(name) ? FlagPermissions.FlagState.TRUE : FlagPermissions.FlagState.FALSE);
-            } else {
-                TempPermMap.put(name, FlagPermissions.FlagState.NEITHER);
+            if (flags.contains(one.getKey())) {
+                resFlags.put(one.getKey(), one.getValue());
             }
         }
+
+        for (Map.Entry<String, Boolean> one : globalFlags.entrySet()) {
+            String fname = one.getKey();
+
+            Flags flag = Flags.getFlag(fname);
+
+            if (flag != null && !flag.isGlobalyEnabled())
+                continue;
+
+            if (!flags.contains(one.getKey())) {
+                continue;
+            }
+
+            if (resFlags.containsKey(one.getKey()))
+                TempPermMap.put(one.getKey(), resFlags.get(one.getKey()) ? FlagPermissions.FlagState.TRUE : FlagPermissions.FlagState.FALSE);
+            else
+                TempPermMap.put(one.getKey(), FlagPermissions.FlagState.NEITHER);
+        }
+
         TempPermMap.remove("admin");
-        return new HashMap<>(TempPermMap);
+
+        return sortByKeyASC(TempPermMap);
     }
 
+    /**
+     * 获取领地玩家权限
+     * 参考实现 com.bekvon.bukkit.residence.gui.setFlagInfo#recalculatePlayer()
+     *
+     * @param player       操作者
+     * @param targetPlayer 目标玩家
+     * @param residence    领地
+     * @return 领地玩家权限列表
+     */
     public static HashMap<String, FlagPermissions.FlagState> getResidencePlayerFlags(Player player, String targetPlayer, ClaimedResidence residence) {
-        if (residence == null) return new HashMap<>();
         Map<String, Boolean> globalFlags = new HashMap<>();
         for (Flags oneFlag : Flags.values()) {
             globalFlags.put(oneFlag.toString(), oneFlag.isEnabled());
         }
-        List<String> flags = residence.getPermissions().getPosibleFlags(player, false, false);
+
+        // TODO: 检查res admin项
+        List<String> flags = residence.getPermissions().getPossibleFlags(player, false, false);
+
         Map<String, Boolean> resFlags = new HashMap<>();
+
         for (Map.Entry<String, Boolean> one : residence.getPermissions().getFlags().entrySet()) {
-            if (flags.contains(one.getKey())) resFlags.put(one.getKey(), one.getValue());
+            if (flags.contains(one.getKey()))
+                resFlags.put(one.getKey(), one.getValue());
         }
-        Set<String> PossibleResPFlags = FlagPermissions.getAllPosibleFlags();
-        Map<String, Boolean> temp = new HashMap<>();
-        for (String one : PossibleResPFlags) {
-            if (globalFlags.containsKey(one)) temp.put(one, globalFlags.get(one));
-        }
-        globalFlags = temp;
-        Map<String, Boolean> pFlags = residence.getPermissions().getPlayerFlags(targetPlayer);
-        if (pFlags != null) resFlags.putAll(pFlags);
-        LinkedHashMap<String, FlagPermissions.FlagState> TempPermMap = new LinkedHashMap<>();
-        for (Map.Entry<String, Boolean> one : globalFlags.entrySet()) {
-            if (!flags.contains(one.getKey())) continue;
-            if (resFlags.containsKey(one.getKey())) {
-                TempPermMap.put(one.getKey(), resFlags.get(one.getKey()) ? FlagPermissions.FlagState.TRUE : FlagPermissions.FlagState.FALSE);
-            } else {
-                TempPermMap.put(one.getKey(), FlagPermissions.FlagState.NEITHER);
+
+        if (targetPlayer != null) {
+
+            Set<String> possibleResPFlags = FlagPermissions.getAllPossibleFlags();
+            Map<String, Boolean> temp = new HashMap<>();
+            for (String one : possibleResPFlags) {
+                if (globalFlags.containsKey(one)) {
+                    temp.put(one, globalFlags.get(one));
+                }
+
+            }
+            globalFlags = temp;
+
+            Map<String, Boolean> pFlags = residence.getPermissions().getPlayerFlags(targetPlayer);
+
+            if (pFlags != null) {
+                resFlags.putAll(pFlags);
             }
         }
-        return new HashMap<>(TempPermMap);
+
+        LinkedHashMap<String, FlagPermissions.FlagState> TempPermMap = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Boolean> one : globalFlags.entrySet()) {
+            if (!flags.contains(one.getKey()))
+                continue;
+
+            if (resFlags.containsKey(one.getKey()))
+                TempPermMap.put(one.getKey(), resFlags.get(one.getKey()) ? FlagPermissions.FlagState.TRUE : FlagPermissions.FlagState.FALSE);
+            else
+                TempPermMap.put(one.getKey(), FlagPermissions.FlagState.NEITHER);
+        }
+
+        return sortByKeyASC(TempPermMap);
+
+
+    }
+
+    /**
+     * 比 Residence.getInstance().getSortingManager().sortByKeyASC() 更好且泛型支持的排序
+     *
+     * @param <T>       值类型
+     * @param unsortMap 待排序Map
+     * @return 已排序Map
+     */
+    public static <T> LinkedHashMap<String, T> sortByKeyASC(Map<String, T> unsortMap) {
+        return unsortMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
     }
 
     public static int flagToInt(FlagPermissions.FlagState flag) {
@@ -115,8 +195,6 @@ public class ResidenceUtils {
     public static void kickPlayer(String targetPlayer, ClaimedResidence residence) {
         Player player = PlayerUtils.getPlayerExtract(targetPlayer);
         if (player == null) return;
-        if (!residence.getPlayersInResidence().contains(player)) return;
-        player.closeInventory();
-        player.teleport(residence.getOutsideFreeLoc(player.getLocation(), player));
+        residence.kickFromResidence(player);
     }
 }
